29014febe3a6593e164f04ece7b99362dd5c9baf
Petar Ginic <petarginic@gmail.com>
2022-09-29 20:57:51 +0200

    done with few updates in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,12 +21,14 @@ const About = () => {
                     <div class="grid md:grid-cols-3 gap-6 justify-items-center items-center">
                         <div class="col-span-2">
                             <p className="text-gray-400 font-bold ">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, maiores nostrum? Ab,
-                                totam? Iste aspernatur adipisci accusantium atque tempore provident veniam unde sunt
-                                consequatur ratione obcaecati, ut, soluta at et. Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Impedit officiis unde provident autem ipsam illum quae maxime iste
-                                odit nobis ex veritatis quia soluta, doloremque, earum, laborum atque odio dolor. Lorem,
-                                ipsum dolor sit amet consectetur
+                                Hello, my name is Petar, I am 29 years old, and I live in the Belgrade. In 2019. I
+                                graduated at MEF faculty and earned a title of Informatics engineer.I am occupied with
+                                IT development, learning new programming languages and training. In future, I see myself
+                                as a full stack developer and always seeking for more and more knowledge and skills.
+                                Although programming is a field that advances day by day and constantly improves, I
+                                enjoy researching and learning new things, and these novelties motivate me further.
+                                Except programming, I have a wide range of interests like gym and street workout,
+                                football, solving puzzle games,traveling..
                             </p>
                         </div>
                         <div class="...">
@@ -41,19 +43,20 @@ const About = () => {
 
                 <div className="flex flex-col items-center">
                     <h1 className="text-gray-300 border-b-4 text-center border-b-[#982d4f] font-bold text-[20px] md:text-4xl  mt-20 ">
-                        Education and Work experiance
+                        Education
                     </h1>
                 </div>
 
                 <div className="max-w-[1000px] mx-auto mt-10">
                     <div class="flex justify-center items-center">
                         <p className="text-gray-400 font-bold ">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, maiores nostrum? Ab,
-                            totam? Iste aspernatur adipisci accusantium atque tempore provident veniam unde sunt
-                            consequatur ratione obcaecati, ut, soluta at et. Lorem ipsum dolor sit amet consectetur
-                            adipisicing elit. Impedit officiis unde provident autem ipsam illum quae maxime iste odit
-                            nobis ex veritatis quia soluta, doloremque, earum, laborum atque odio dolor. Lorem, ipsum
-                            dolor sit amet consectetur
+                            In 2011, I encountered programming for the first time, more precisely with a programming
+                            language C. At university I encountered other languages like Java,PHP, Python. In 2019, I'm
+                            returning to programming, more precisely to what I love the most. I finished the course
+                            "Learning Python", "Python Advanced with Flask" on Udemy, but I wanted to all with Frontend.
+                            In 2021. started learning ReactJS. Finished a course "ReactJS advanced - Udemy" and
+                            "Frontend Web Development Bootcamp Course". Currently I am on Quantox academy, eager to
+                            establish and consolidate knowledge. This website is made using ReactJS.
                         </p>
                     </div>
                 </div>
